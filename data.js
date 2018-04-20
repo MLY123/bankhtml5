dc601511b486ce78813bb0ac582ca7e86f19b864
MLY123 <1753868366@qq.com>
2018-04-20 09:10:41 +0800

    12

---
+++ data.js
@@ -0,0 +1,35 @@
+// data = {
+//     name:['1','2','3'],
+//     sum:['a','b','c'],
+//     de:['121','fsfs','sdadas'],
+//     url:['html1','html2','html3']
+// };
+// console.log(data.name.length);
+// var strdata = '';
+// for(var i=0;i<data.name.length;i++) {
+//     strdata +='<div class="swiper-slide">';
+//     strdata +='<a class="to-swiper" href="'+data.url[i]+'">';
+//     strdata +='<div style="width:10px;"><img src="'+data.url[i]+'"></div>';
+//     strdata +='<div class="banner-title">';
+//     strdata +='<h4>'+data.name[i]+'</h4>';
+//     strdata +='<p>'+data.sum[i]+'</p>';
+//     strdata +='</div>';
+//     strdata +='</a>';
+//     strdata +='</div>';
+//     $(".swiper-wrapper").html(strdata);
+// }
+
+// <div class="swiper-slide">
+//     <a class="to-swiper" href="html5/swiper.html?product=banner_xiaohua-ningqingren&key=https://www.xhqb.com/m/ffpp.html?appChannel=shengb05">
+//           <div class="banner-img">
+//             <img src="Img/banner_adv/xiaohuaqianbao.jpg">
+//           </div>
+//           <div class="banner-title">
+//              <h4>小花钱包-年轻人的互联网钱包</h4>
+//              <p>最高5万额度，自动审批，实时到账</p>
+//
+//          </div>
+//     </a>
+//
+//
+// </div>
